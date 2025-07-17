0ca45b235e368c2aaef8ec7300aecb13d307593d
Leonid <gyw@protonmail.com>
2025-07-17 11:38:36 +0300

    ver.1.2 pre nginx conf added

--- nginx/init-certbot.sh
+++ nginx/init-certbot.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-
 set -e
 
 CERT_PATH="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
 
+# Сначала запустим nginx с pre-cert конфигом
+echo ">>> Запускаем nginx с pre-cert конфигом..."
+cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.bak
+cp /etc/nginx/conf.d/pre-cert.conf /etc/nginx/conf.d/default.conf
+nginx &
+
+# Ждём, пока nginx поднимется
+sleep 5
+
 if [ "$ENV" = "production" ] && [ ! -f "$CERT_PATH" ]; then
-  echo ">>> Сертификат не найден. Запускаем Certbot..."
-  
+  echo ">>> Получаем сертификат..."
   certbot certonly \
     --webroot -w /var/www/certbot \
-    --email "$EMAIL" \
-    --agree-tos --no-eff-email \
+    --email "$EMAIL" --agree-tos --no-eff-email \
     -d "$DOMAIN"
-
-  echo ">>> Сертификат получен. Перезапускаем nginx..."
-  nginx -s reload
-else
-  echo ">>> Сертификат уже существует или не production. Запускаем nginx..."
 fi
 
-# Запускаем nginx в foreground
+# Останавливаем nginx и запускаем с боевым конфигом
+echo ">>> Перезапускаем nginx с production конфигом..."
+nginx -s stop
+cp /etc/nginx/conf.d/production.conf /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
