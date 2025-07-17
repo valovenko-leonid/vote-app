acce0e628f9f3e80c485f4fc342b784200972157
Leonid <gyw@protonmail.com>
2025-07-17 13:05:47 +0300

    asd

--- nginx/init-certbot.sh
+++ nginx/init-certbot.sh
@@ -16,14 +16,14 @@ fi
 # Если сертификат уже есть — запускаем nginx с боевым конфигом
 if [ -f "$CERT_PATH" ]; then
   echo ">>> Сертификат найден. Запускаем nginx с default.conf..."
-  cp "$DEFAULT_CONF" /etc/nginx/conf.d/active.conf
+  cp "$DEFAULT_CONF" /etc/nginx/conf.d/default.conf
   nginx -g "daemon off;"
   exit 0
 fi
 
 # Если сертификата нет — сначала запускаем nginx с pre-cert конфигом
 echo ">>> Сертификат не найден. Запускаем nginx с pre-cert конфигом..."
-cp "$PRECERT_CONF" /etc/nginx/conf.d/active.conf
+cp "$PRECERT_CONF" /etc/nginx/conf.d/default.conf
 nginx
 
 echo ">>> Пытаемся получить сертификат..."
@@ -31,7 +31,7 @@ if certbot certonly --webroot -w /var/www/certbot \
   --email "$EMAIL" --agree-tos --no-eff-email -d "$DOMAIN"; then
 
   echo ">>> Сертификат успешно получен. Перезапускаем nginx с default.conf..."
-  cp "$DEFAULT_CONF" /etc/nginx/conf.d/active.conf
+  cp "$DEFAULT_CONF" /etc/nginx/conf.d/default.conf
   nginx -s reload
 else
   echo ">>> Не удалось получить сертификат. Остаёмся на pre-cert.conf"
